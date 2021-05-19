6670693b444f38705d0ef460d5e722dc7d8647b5
jjayjack <jocelynjayjack@gmail.com>
2021-05-19 16:44:28 -0500

    Updated folder

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Endurance = require("../models/endurance.js");
+const Strength = require ("../models/strength.js")
+
+router.post("/api/endurance", ({ body }, res) => {
+  Endurance.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.post("/api/strength", ({ body }, res) => {
+  Strength.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
