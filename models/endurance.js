d21bd81c9abf88acd1b57fb5ba621b1544653e4a
jjayjack <jocelynjayjack@gmail.com>
2021-05-19 17:24:48 -0500

    Updated model to include specific to model

--- models/endurance.js
+++ models/endurance.js
@@ -3,19 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const enduranceSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Enter a type for exercise",
-  },
-  name: {
-    type: String,
-    required: "Enter a name for the exercise"
-  },
-  duration: {
-    type: Number,
-    required: "Enter amount of time",
-  },
   distance: {
       type: Number,
       required: "Enter the weight used for the exercise"
