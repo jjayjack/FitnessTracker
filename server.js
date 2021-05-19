7d8596b6321512b6a04e80245d3f701601c944e1
jjayjack <jocelynjayjack@gmail.com>
2021-05-19 17:24:26 -0500

    Updated routes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs");
 
 const PORT = process.env.PORT || 3000
 
+const Workout = require('./models/exercise.js')
 const app = express();
+
+const databaseUrl = "workout";
+const collections = ["endurance", "strength"];
+
+const db = mongojs(databaseUrl, collections);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +21,16 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+app.get('/exercise', (req, res) => {
+  db.exercise.find({}, (err, found) => {
+    if (err){
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  })
+})
 
 // routes
 app.use(require("./routes/api.js"));
